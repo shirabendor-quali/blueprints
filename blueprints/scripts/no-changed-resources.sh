600049536d313f326f69ae154b79caf8c98a2ef9
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2023-11-07 17:49:31 +0200

    Update no-changed-resources.sh

--- blueprints/scripts/no-changed-resources.sh
+++ blueprints/scripts/no-changed-resources.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
 echo "$TORQUE_TF_PLAN_PATH"
-value=`cat "$TORQUE_TF_PLAN_PATH" | tr -d '\0'`
-echo $value
-
-if grep -q "0 to change" $value ; then
-  echo "Success: No changed resources."
-  exit 0
-else
-  echo "Error: The plan indicates modified resources"
-  exit 1
-fi
 
 
