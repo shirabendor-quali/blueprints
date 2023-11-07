e66a1823e7e38fdce6a94c7acab40cc050a8775f
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2023-11-07 15:34:51 +0200

    Update no-changed-resources.sh

--- blueprints/scripts/no-changed-resources.sh
+++ blueprints/scripts/no-changed-resources.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if grep -q 'Plan: [1-9][0-9]* to add, 0 to change, 0 to destroy' "$TORQUE_TF_PLAN_PATH"; then
-  echo "Error: The plan indicates new resources"
-  exit 1
-else
-  echo "Success: No new resources added."
+if grep -q 'Plan: [0-9]* to add, 0 to change, [0-9]* to destroy' "$TORQUE_TF_PLAN_PATH"; then
+  echo "Success: No changed resources."
   exit 0
+else
+  echo "Error: The plan indicates nodified resources"
+  exit 1
 fi
